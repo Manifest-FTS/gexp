0bca80a0115284ebbb11fa2c455606fa4176803e
DevKev <devkev@manifestfts.com>
2023-12-19 19:19:28 -0500

    updated darkmode logos

--- src/components/ui/logo-icon.tsx
+++ src/components/ui/logo-icon.tsx
@@ -2,7 +2,7 @@ import Image from '@/components/ui/image';
 import { useIsMounted } from '@/lib/hooks/use-is-mounted';
 import { useIsDarkMode } from '@/lib/hooks/use-is-dark-mode';
 import lightLogo from '@/assets/images/logo-icon.svg';
-import darkLogo from '@/assets/images/logo-icon-white.png';
+import darkLogo from '@/assets/images/logo-icon-white.svg';
 
 const Logo: React.FC<React.SVGAttributes<{}>> = (props) => {
   const isMounted = useIsMounted();
